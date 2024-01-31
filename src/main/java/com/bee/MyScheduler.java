f5d385a7fb7baeccdb5b0f8e0af5902b76020157
Aidan Border <planes4u@outlook.com>
2024-01-31 13:29:03 -0500

    Add semaphore ig

--- src/main/java/com/bee/MyScheduler.java
+++ src/main/java/com/bee/MyScheduler.java
@@ -1,4 +1,13 @@
 package com.bee;
 
+import java.util.concurrent.Semaphore;
+
 public class MyScheduler {
+  Semaphore semaphore;
+
+  public MyScheduler(Semaphore semaphore) {
+    this.semaphore = semaphore;
+  }
+
+
 }
