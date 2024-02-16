c50f5a84c0dab789d0bc8a98b39e195b1046edde
Aidan Border <planes4u@outlook.com>
2024-02-16 13:38:47 -0500

    Adding some methods to MyBlockingQueue
    Also genericized the class

--- src/main/java/com/bee/MyBlockingQueue.java
+++ src/main/java/com/bee/MyBlockingQueue.java
@@ -2,11 +2,19 @@ package com.bee;
 
 import java.util.concurrent.Semaphore;
 
-public class MyBlockingQueue {
+public class MyBlockingQueue<T> {
   Semaphore semaphore;
 
   public MyBlockingQueue(Semaphore semaphore) {
     this.semaphore = semaphore;
+  }
+
+  public void add(Object object) {
+    
+  }
+
+  public void remove(Object object) {
+
   }
 
 
