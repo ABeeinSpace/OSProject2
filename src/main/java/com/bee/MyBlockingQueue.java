01188933e61c355a1cc1e07106c2065d2e4e1de2
Aidan Border <planes4u@outlook.com>
2024-02-16 14:19:33 -0500

    Adding methods and class shiz

--- src/main/java/com/bee/MyBlockingQueue.java
+++ src/main/java/com/bee/MyBlockingQueue.java
@@ -1,21 +1,38 @@
 package com.bee;
 
+import java.util.*;
 import java.util.concurrent.Semaphore;
-
+/*
+ *
+ * Models a thread-safe Blocking Queue
+ *
+ * @author Aidan Border
+ * @author Quade
+ *
+ */
 public class MyBlockingQueue<T> {
   Semaphore semaphore;
+  int maxNumElements;
+  Queue<T> elementsQueue; 
 
-  public MyBlockingQueue(Semaphore semaphore) {
-    this.semaphore = semaphore;
+  public MyBlockingQueue(int maxNum) {
+    int maxNumElements = maxNum;
+    elementsQueue = new LinkedList<>();
+    semaphore = new Semaphore(maxNum);
+
   }
 
-  public void add(Object object) {
+  public synchronized void add(T element) {
     
   }
 
-  public void remove(Object object) {
-
+  public synchronized T remove() {
+    
   }
 
+  @Override
+  public String toString() {
+    return "MyBlockingQueue []";
+  }
 
 }
